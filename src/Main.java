e58cc80642a684a90483f7a97d2fabbfbdadb2a9
ryan23052008 <ryandiasvieira123@gmail.com>
2025-03-22 10:16:38 -0300

    feat:Aula java

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String nome = "Ryan";
+        System.out.println("Meunome é:" + nome);
+        System.out.println();
+
+
+        char status = 'R';
+        char status01 = 'Y';
+        char status02 = 'A';
+        char status03 = 'N';
+        System.out.println(status + status01 + status02 + status03);
+        System.out.println("" + status + status01 + status02 + status03);
+
+        System.out.println();
+
+       // Float num02 = 10.5f;
+       // Double num03 = 10.5;
+
+        //System.out.println(num + num04);
+       // System.out.println(num - num04);
+       // System.out.println(num / num04);
+       // System.out.println(num % num04);
+
+        System.out.println();
+
+        int num = 30;
+        Long num04 = 50L;
+        byte numNovo = 128;
+        byte numNovo02 = 127;
+
+        System.out.println(num + num04);
+        System.out.println(num - num04);
+        System.out.println(num / num04);
+        System.out.println(num % num04);
+
+        System.out.println();
+
+
+
+
+
+
+
+    }
+    }
